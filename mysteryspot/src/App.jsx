acb0394036679a16c7f133b03fc80ee93081cb41
Gadekar2003 <gadekarshrutika98@gmail.com>
2025-08-17 22:00:01 +0530

    hidden object

--- mysteryspot/src/App.jsx
+++ mysteryspot/src/App.jsx
@@ -1,8 +1,23 @@
-import {useState} from "react"
-import LoadingPage from "./pages/LoadingPage"
-import MenuPage from "./pages/MenuPage"
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoadingPage from "./pages/LoadingPage";
+import MenuPage from "./pages/MenuPage";
+import GameOver from "./pages/GameOverPage";
 
-export default function App(){
-	const[loaded,setLoaded]=useState(false)
-	return loaded?<MenuPage/>:<LoadingPage onFinish={()=>setLoaded(true)}/>
+export default function App() {
+  const [loaded, setLoaded] = useState(false);
+
+  if (!loaded) {
+    return <LoadingPage onFinish={() => setLoaded(true)} />;
+  }
+
+  return (
+    
+      <Routes>
+        <Route path="/menu" element={<MenuPage />} />
+        <Route path="/gameover" element={<GameOver/>} />
+        <Route path="*" element={<MenuPage />} /> {/* Default */}
+      </Routes>
+    
+  );
 }
