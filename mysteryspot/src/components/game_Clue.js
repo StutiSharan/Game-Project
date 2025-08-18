7062f78e4af4bb02bdd0ed213f8326cb330e59b8
Yash-Bisht <whyrush711@gmail.com>
2025-08-18 18:55:49 +0530

    some minor updates in game

--- mysteryspot/src/components/game_Clue.js
+++ mysteryspot/src/components/game_Clue.js
@@ -1,24 +1,24 @@
 const myClues = {
   1: [
-    { id: 1, x: 810, y: 50, found: false, hint: "A colorful flyer dances in the sky." }, // Kite
-    { id: 3, x: 900,  y: 430, found: false, hint: "A blue ring awaits a splash." }, // Inflatable Pool
-    { id: 6, x: 1040,  y: 625, found: false, hint: "A yellow fowl, not real, but cute." }, // Duck
-    { id: 7, x: 1180, y: 756, found: false, hint: "A red fruit, dropped from the hand." }, // Apple
-    { id: 9, x: 1140, y: 856, found: false, hint: "Dark lenses shield eyes on checkered cloth." }, // Sunglasses
+    { id: 1, x: 850, y: 80, found: false, hint: "A colorful flyer dances in the sky." }, // Kite
+    { id: 3, x: 950,  y: 460, found: false, hint: "A blue ring awaits a splash." }, // Inflatable Pool
+    { id: 6, x: 1090,  y: 685, found: false, hint: "A yellow fowl, not real, but cute." }, // Duck
+    { id: 7, x: 1230, y: 786, found: false, hint: "A red fruit, dropped from the hand." }, // Apple
+    { id: 9, x: 1190, y:920, found: false, hint: "Dark lenses shield eyes on checkered cloth." }, // Sunglasses
   ],
   4: [
-    { id: 1, x: 1163, y: 598, found: false, hint: "A watchful gaze from a furry friend." }, // Cat
-    { id: 5, x: 698, y: 8, found: false, hint: "A small goddess model, showing power." }, // goddess model
-    { id: 6, x: 1016, y: 18, found: false, hint: "A blue circle tells where wheels may roll." }, // Bicycle Sign
-    { id: 8, x: 1066,  y: 186, found: false, hint: "A glowing head of something, motionless but illuminating." }, // lamp
-    { id: 9, x: 986, y: 486, found: false, hint: "An eye to capture hidden scenes." }, // Camera
+    { id: 1, x: 1230 , y: 640, found: false, hint: "A watchful gaze from a furry friend." }, // Cat
+    { id: 5, x: 788, y: 58, found: false, hint: "A small goddess model, showing power." }, // goddess model
+    { id: 6, x: 1036, y: 78, found: false, hint: "A blue circle tells where wheels may roll." }, // Bicycle Sign
+    { id: 8, x: 1106,  y: 240, found: false, hint: "A glowing head of something, motionless but illuminating." }, // lamp
+    { id: 9, x: 1028, y: 560, found: false, hint: "An eye to capture hidden scenes." }, // Camera
   ],
   6: [
-    { id: 1, x: 653,  y: 648, found: false, hint: "A clear eye for tiny details." }, // Magnifying Glass
-    { id: 2, x: 998, y: 680, found: false, hint: "A single dark heart, laid upon the table." }, // Ace of Spades
-    { id: 8, x: 929,  y: 386, found: false, hint: "A spiny sentinel in a red pot." }, // Cactus
-    { id: 9, x: 950,  y: 214, found: false, hint: "An Owl modle, staring staright to eyes of yours." }, // owls
-    { id: 10, x: 938,  y: 625, found: false, hint: "A tube of color for a final touch." }, // Lipstick
+    { id: 1, x: 693,  y: 688, found: false, hint: "A clear eye for tiny details." }, // Magnifying Glass
+    { id: 2, x: 909, y: 496, found: false, hint: "A layer by layer thing that we can eat" }, // Sandwich
+    { id: 8, x: 969,  y: 426, found: false, hint: "A spiny sentinel in a red pot." }, // Cactus
+    { id: 9, x: 990,  y: 264, found: false, hint: "An Owl modle, staring staright to eyes of yours." }, // owls
+    { id: 10, x: 988,  y: 685, found: false, hint: "A tube of color for a final touch." }, // Lipstick
   ],
 };
 
