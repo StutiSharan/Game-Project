dbf82d2a758985c873493469a3eaf0d693d225c0
Yash-Bisht <whyrush711@gmail.com>
2025-08-18 12:46:23 +0530

    some minor updates

--- mysteryspot/src/components/scoring.js
+++ mysteryspot/src/components/scoring.js
@@ -1,38 +1,6 @@
-import { db } from "./firebase"; // your firebase config
+import { db } from "../firebase/firebase"; // your firebase config
 import { doc, setDoc, getDoc } from "firebase/firestore";
 
-// // Save or Update Best Score
-// export async function saveBestScore(username, level, newScore,time) {
-//   try {
-//     const scoreRef = doc(db, "users", username, "scores", level);
-
-//     // get current score
-//     const scoreSnap = await getDoc(scoreRef);
-
-//     if (scoreSnap.exists()) {
-//       const currentBest = scoreSnap.data().bestScore;
-
-//       // update only if new score is higher
-//       if (newScore > currentBest) {
-//         await setDoc(scoreRef, { bestScore: newScore }, { merge: true });
-//         console.log("✅ Best score updated!");
-//       } else {
-//         console.log("⚡ Not higher than current best, no update.");
-//       }
-//     } else {
-//       // if score does not exist, create new
-//       await setDoc(scoreRef, { bestScore: newScore });
-//       console.log("✅ New score saved!");
-//     }
-//   } catch (error) {
-//     console.error("Error saving score:", error);
-//   }
-// }
-
-// import { db } from "./firebase"; // your firebase config
-// import { doc, setDoc, getDoc } from "firebase/firestore";
-
-// Save or Update Best Score with Time
 export async function saveBestScore(username, level, newScore, time) {
   try {
     const scoreRef = doc(db, "users", username, "scores", level);
--- mysteryspot/src/pages/Game.jsx
+++ mysteryspot/src/pages/Game.jsx
@@ -1,4 +1,5 @@
 //..............................................................................................
+//see Comment to find any particular gaming related stuff
 import React, { useState, useEffect, useRef } from "react";
 import { motion } from "framer-motion";
 import { Button } from "../components/ui/Button";
